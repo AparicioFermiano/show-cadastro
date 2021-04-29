69850e0713e97b439970fabadd0c7a90d54c0cad
AparicioFermiano <aparicio.empresarial@gmail.com>
2021-04-29 19:29:47 -0300

    Melhoria do código

--- src/index.js
+++ src/index.js
@@ -3,22 +3,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const clientes = [
-    {
-        id: 1,
-        nome: "João",
-        endereco: "Rua Vicente Lopes",
-        idade: 35
-        },
-        
-        {
-        id: 2,
-        nome: "Marcela",
-        endereco: "Rua Batista",
-        idade: 23
-    }
-];
-id = 2;
+const clientes = [];
+
+id = 0;
 
 app.get('/clientes', (req, res) => {
     res.send(clientes);
@@ -26,15 +13,10 @@ app.get('/clientes', (req, res) => {
 
 app.post('/clientes', (req, res) => {
     
-    const info = req.body;
-    const informacao = {
-        id: id += 1,
-        nome: info.nome,
-        endereco: info.endereco,
-        idade: info.idade,
-    }
-    clientes.push(informacao) 
-    res.status(201).send(clientes);
+    const {nome, endereco, idade } = req.body
+    id ++
+    clientes.push({id, nome, endereco, idade})
+    res.status(201).send(clientes)
 });
 app.put('/clientes', (req, res) => {
     clientes.forEach((informacao) => {
