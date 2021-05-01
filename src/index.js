a6adc4d685603049e8eac6cd7a9de7a02cbd6b4d
AparicioFermiano <aparicio.empresarial@gmail.com>
2021-05-01 14:45:52 -0300

    v1.1

--- src/index.js
+++ src/index.js
@@ -2,45 +2,58 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const axios = require('axios');
+const port = 2000
 
-const clientes = [];
+const clientes = [{
+    "id": 1,
+    "nome": "Jully",
+    "endereco":"Rua cracolandia",
+    "idade": 74
+    },
+    {
+    "id": 2,
+    "nome": "Marcelo",
+    "endereco":"Rua Duende",
+    "idade": 23
+    }];
 
-id = 0;
+id = 2;
 
 app.get('/clientes', (req, res) => {
     res.send(clientes);
 });
 
-app.post('/clientes', (req, res) => {
+app.post('/clientes', async (req, res) => {
+    const { nome, endereco, idade } = req.body;
+    id ++;
+    clientes.push({id, nome, endereco, idade});
+    axios.post('http://localhost:10000/eventos', {
+        tipo: "ObservacaoAtualizada",
+        dados: {
+            id, nome, endereco, idade
+        }
+    });
     
-    const {nome, endereco, idade } = req.body
-    id ++
-    clientes.push({id, nome, endereco, idade})
-    res.status(201).send(clientes)
-});
-app.put('/clientes', (req, res) => {
-    clientes.forEach((informacao) => {
-        informacao.id === req.body.id ?
-        (informacao.nome = req.body.nome,
-        informacao.endereco = req.body.endereco,
-        informacao.idade = req.body.idade) : "";
-    })
     res.status(200).send(clientes)
 })
 
-app.delete('/clientes/:id', (req, res, next) => {
+app.post('/eventos', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok"})
+})
+
+app.delete('/clientes/:id', async (req, res, next) => {
     var idcliente  = req.params.id;
     clientes.forEach((informacao, index) => {
         (informacao.id == idcliente) ? clientes.splice(index, 1) : ""
     })
+    axios.post('http://localhost:10000/eventos', {
+        tipo: "ArquivoDeletado",
+    });
      res.status(200).send(clientes);
 });
 
-
-
-app.listen(2000, () => {
+app.listen(port, () => {
     console.log('Clientes. Porta 2000');
 });
-
-
-
