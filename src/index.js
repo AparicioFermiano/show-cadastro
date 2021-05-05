2b9cfac78cbad650f6a92614e066c8d5521da35c
AparicioFermiano <aparicio.empresarial@gmail.com>
2021-05-05 17:29:50 -0300

    Atualização de cadastro funcionando

--- src/index.js
+++ src/index.js
@@ -5,55 +5,76 @@ app.use(express.json());
 const axios = require('axios');
 const port = 2000
 
-const clientes = [{
-    "id": 1,
-    "nome": "Jully",
-    "endereco":"Rua cracolandia",
-    "idade": 74
-    },
-    {
-    "id": 2,
-    "nome": "Marcelo",
-    "endereco":"Rua Duende",
-    "idade": 23
-    }];
 
-id = 2;
+const clientes = []
 
+id = 0;
+//Obter a informação contida no json
 app.get('/clientes', (req, res) => {
-    res.send(clientes);
+    res.status(200).send(clientes);
 });
-
+//Criação de novo usuário
+//async significa assincrona, envia porém não espera resposta
 app.post('/clientes', async (req, res) => {
+    //Requiremento do corpo do Json
     const { nome, endereco, idade } = req.body;
     id ++;
+    //Inserir conteúdo no json
     clientes.push({id, nome, endereco, idade});
+    //Enviar uma mensagem sem retorno para o barramento
     axios.post('http://localhost:10000/eventos', {
-        tipo: "ObservacaoAtualizada",
+        tipo: "clienteCriado",
         dados: {
             id, nome, endereco, idade
         }
-    });
+    });  
+    //retornar o resultado do json atualizado
+    res.status(200).send(clientes)
+})
+app.put('/clientes/:id', (req, res) => {
+    const { nome, endereco, idade } = req.body;
+    const idCliente = req.params.id || [];
+    clientes.forEach((informacao) => {
+    idCliente == informacao.id ?
+    (informacao.nome = nome,
+    informacao.endereco = endereco,
+    informacao.idade = idade,
+    axios.post("http://localhost:10000/eventos", {
+        tipo: "Cadastro Atualizado",
+        dados: {nome: informacao.nome, 
+                endereco: informacao.endereco, 
+                idade: informacao,idade},
+        })) : "";
+    })
     
     res.status(200).send(clientes)
 })
 
+//Receber informação do barramento
 app.post('/eventos', (req, res) => {
+    try{
     console.log(req.body);
+    } catch(err){}       
     res.status(200).send({ msg: "ok"})
 })
-
-app.delete('/clientes/:id', async (req, res, next) => {
+//Realizar um delete em um cliente cadastrado
+app.delete('/clientes/:id', async (req, res) => {
+    //Recebe o parametros da id da URL em uma variavel
+    const { nome, endereco, idade } = req.body;
     var idcliente  = req.params.id;
+    //faz um laço de repetição, buscando o id do cliente e faz o delete.
     clientes.forEach((informacao, index) => {
         (informacao.id == idcliente) ? clientes.splice(index, 1) : ""
-    })
+    })//Envia a informação do arquivo deletado para o barramento
     axios.post('http://localhost:10000/eventos', {
-        tipo: "ArquivoDeletado",
+        tipo: "ClienteDeletado",
+        dados: {
+            id
+        }
     });
      res.status(200).send(clientes);
 });
-
+//Faz a listagem da porta
 app.listen(port, () => {
-    console.log('Clientes. Porta 2000');
+    console.log('Cadastro. Porta 2000');
 });
